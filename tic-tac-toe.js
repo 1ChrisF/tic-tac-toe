687e104275f9631826e02c4124c909ac75b634a6
1chrisf <3chrisf@gmail.com>
2021-09-09 22:10:40 +0100

    added players

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,38 +1,43 @@
-const Player =
-
-    playerToken = [1,-1]
-getToken = function
-
-    //give player right token
-//return {getToken}
-//
-
-const player1 = Player(getToken);
-const player2 = Player(getToken);
-
-const gameBoard =
-    boardState =
-           [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
-
-//listen for click 
-//
-// take data x and data y
-//check for zero in arrays /return
-//check value of player 
-//change value of arr at xy to 1/-1
-// look for arry that = 3
-//increase turn count
-//check move number
-//if 9 and no 3/-3 draw
-
-//swictch player
 
 
+const Player = (mark) => {
+    
+    let score = score
+    const markValue = [1, -1];
+    const playerMark = ["o", "x"];
+    getMark = () => playerMark[mark];
+    getMarkValue = () => markValue[mark];
+
+    return { getMark, getMarkValue };
+}
+
+const player0 = Player(0);
+const player1 = Player(1);
 
 
-const displayControler
+const gameBoard = () => {
+    let boardState =
+        [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]];
+
+    //listen for click 
+    //
+    // take data x and data y
+    //check for zero in arrays /return
+    //check value of player 
+    //change value of arr at xy to 1/-1
+    // look for arry that = 3
+    //increase turn count
+    //check move number
+    //if 9 and no 3/-3 draw
+
+    //swictch player
+
+}
+
+
+const displayControler = 0;
 // take data x and data y
 //check active player 
 //add image to cord
