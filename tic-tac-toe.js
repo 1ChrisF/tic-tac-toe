d1fc6f391b8504f39ae99c9bf032b83588044e6c
1chrisf <3chrisf@gmail.com>
2021-09-09 20:46:16 +0100

    skeloton files

---
+++ tic-tac-toe.js
@@ -0,0 +1,41 @@
+const Player =
+
+    playerToken = [1,-1]
+getToken = function
+
+    //give player right token
+//return {getToken}
+//
+
+const player1 = Player(getToken);
+const player2 = Player(getToken);
+
+const gameBoard =
+    boardState =
+           [[0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]]
+
+//listen for click 
+//
+// take data x and data y
+//check for zero in arrays /return
+//check value of player 
+//change value of arr at xy to 1/-1
+// look for arry that = 3
+//increase turn count
+//check move number
+//if 9 and no 3/-3 draw
+
+//swictch player
+
+
+
+
+const displayControler
+// take data x and data y
+//check active player 
+//add image to cord
+
+//clear button 
+//remove all images 
